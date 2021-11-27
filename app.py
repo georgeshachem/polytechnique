aec9c4825489ce3e36ced18ee39be6dd56cdc76b
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2021-11-27 15:03:12 +0100

    added database

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from database import db
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
+
+db.init_app(app)
--- config.py
+++ config.py
@@ -2,15 +2,4 @@ class Config(object):
     DEBUG = True
     TESTING = True
     SECRET_KEY = 'my_super_secret_key'
-
-
-class ProductionConfig(Config):
-    DEBUG = False
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost/georges"
