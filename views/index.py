c3cb49a75ac8a5e56e41d5c549f752f3e466148d
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2021-11-27 15:03:27 +0100

    updated main page to select database

--- views/index.py
+++ views/index.py
@@ -1,7 +1,22 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
+from database import db
 
 
 @app.route("/")
 def index():
-    return "Index"
+    db_name = request.args.get("q")
+    if db_name:
+        return db_name
+    else:
+        all_db_names = [x[0] for x in db.engine.execute(
+            'SELECT datname FROM pg_database;').fetchall()]
+        return render_template('index/index.html', all_db_names=all_db_names)
+
+
+@app.route("/db")
+def db_test():
+    all_db_names = [x[0] for x in db.engine.execute(
+        'SELECT datname FROM pg_database;').fetchall()]
+    print(all_db_names)
+    return all_db_names
