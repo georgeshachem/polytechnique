c0c031cb4ba0ef7a2fb66bb6b318ae1ab60f54eb
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2021-11-27 18:59:02 +0100

    added q5

--- views/q.py
+++ views/q.py
@@ -25,17 +25,81 @@ def q1():
 def q4():
     db_name = request.args.get("d")
     table_name = request.args.get("t")
-    print(db_name, table_name)
     if db_name and table_name:
         con = psycopg2.connect(host=app.config['POSTGRES_HOST'], database=db_name,
                                user=app.config['POSTGRES_USER'], password=app.config['POSTGRES_PASSWORD'], port=app.config['POSTGRES_PORT'])
         try:
-            query = "select c.oid, c.relname, a.amname from pg_class c, pg_namespace n, pg_am a where (c.relkind='r' or c.relkind='i') and n.oid=c.relnamespace and not(nspname like 'pg_%%' or nspname='information schema') and c.relam=a.oid AND c.relname=%s;"
+            query = """select
+                            c.oid,
+                            c.relname,
+                            a.amname
+                        from
+                            pg_class c,
+                            pg_namespace n,
+                            pg_am a
+                        where
+                            (
+                                c.relkind = 'r'
+                                or c.relkind = 'i'
+                            )
+                            and n.oid = c.relnamespace
+                            and not(
+                                nspname like 'pg_%%'
+                                or nspname = 'information schema'
+                            )
+                            and c.relam = a.oid
+                            AND c.relname = %s;"""
             cur = con.cursor()
             cur.execute(query, (table_name, ))
             query_result = cur.fetchall()
         finally:
             con.close()
-        return render_template('results/q4.html', db_name=db_name, query_result=query_result)
+        return render_template('results/q4.html', db_name=db_name, table_name=table_name, query_result=query_result)
+    else:
+        return "No database or table name provided"
+
+
+@app.route("/q5")
+def q5():
+    db_name = request.args.get("d")
+    table_name = request.args.get("t")
+    if db_name and table_name:
+        con = psycopg2.connect(host=app.config['POSTGRES_HOST'], database=db_name,
+                               user=app.config['POSTGRES_USER'], password=app.config['POSTGRES_PASSWORD'], port=app.config['POSTGRES_PORT'])
+        try:
+            query = """with mytables as (
+                        select
+                            c.oid,
+                            c.relname
+                        from
+                            pg_class c,
+                            pg_namespace n
+                        where
+                            c.relkind = 'r'
+                            and n.oid = c.relnamespace
+                            and not(
+                                nspname like 'pg_%%'
+                                or nspname = 'information_schema'
+                            )
+                    )
+                    select
+                        t.relname,
+                        a.attname,
+                        y.typname
+                    from
+                        mytables t,
+                        pg_attribute a,
+                        pg_type y
+                    where
+                        t.oid = a.attrelid
+                        and a.atttypid = y.oid
+                        and y.typname = 'varchar'
+                        and t.relname = %s;"""
+            cur = con.cursor()
+            cur.execute(query, (table_name, ))
+            query_result = cur.fetchall()
+        finally:
+            con.close()
+        return render_template('results/q5.html', db_name=db_name, table_name=table_name, query_result=query_result)
     else:
         return "No database or table name provided"
