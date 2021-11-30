38cf9293423ef9482f4ffcea9a9a84b548460846
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2021-11-30 16:47:06 +0100

    added tooltip to fig1

--- INF552/Project/js/fig1.js
+++ INF552/Project/js/fig1.js
@@ -28,11 +28,16 @@ var loadData = function (svg) {
             }
         }
 
-        // console.log(prices_per_neighbourhood);
-
+        let neighbourhoods_rank = [];
         for (let [key, value] of Object.entries(prices_per_neighbourhood)) {
-            prices_per_neighbourhood[key] = average(value);
+            let neighbourhood_avg = average(value);
+            prices_per_neighbourhood[key] = neighbourhood_avg;
+            neighbourhoods_rank.push({
+                neighbourhood: key,
+                val: neighbourhood_avg,
+            });
         }
+        neighbourhoods_rank = neighbourhoods_rank.sort((a, b) => a.val - b.val);
 
         let values = Object.values(prices_per_neighbourhood);
         let min_value = Math.min(...values);
@@ -44,7 +49,15 @@ var loadData = function (svg) {
             .domain([min_value, mean_value, max_value])
             .range(["#fee8c8", "#fdbb84", "#e34a33"]);
 
-        // console.log(min_value, max_value);
+        // var neighbourhoods_rank = Object.values(prices_per_neighbourhood).map(
+        //     function (elt) {
+        //         return parseInt(elt);
+        //     }
+        // );
+        // neighbourhoods_rank = neighbourhoods_rank.sort(function (a, b) {
+        //     return a - b;
+        // });
+        // console.log(neighbourhoods_rank);
 
         var projection = d3
             .geoMercator()
@@ -53,6 +66,19 @@ var loadData = function (svg) {
             .translate([ctx.w / 2, ctx.h / 2]);
 
         var path = d3.geoPath().projection(projection);
+
+        var tooltip = d3
+            .select("#main")
+            .append("div")
+            .style("position", "absolute")
+            .style("visibility", "hidden")
+            .style("padding-top", "3px")
+            .style("padding-bottom", "3px")
+            .style("padding-left", "3px")
+            .style("padding-right", "3px")
+            .style("border-style", "solid")
+            .style("background", "white")
+            .style("white-space", "pre-line");
 
         // Load external data and boot
         d3.json("../data/paris/neighbourhoods.geojson").then(function (data) {
@@ -66,7 +92,29 @@ var loadData = function (svg) {
                     color(prices_per_neighbourhood[d.properties.neighbourhood])
                 )
                 .attr("d", d3.geoPath().projection(projection))
-                .style("stroke", "black");
+                .style("stroke", "black")
+                .on("mouseover", function (event, d) {
+                    let neighbourhood = d.properties.neighbourhood;
+                    let rank = neighbourhoods_rank
+                        .map(function (e) {
+                            return e.neighbourhood;
+                        })
+                        .indexOf(neighbourhood);
+                    let avg = parseInt(prices_per_neighbourhood[neighbourhood]);
+
+                    tooltip.style("visibility", "visible");
+                    tooltip.text(
+                        `${neighbourhood} \n Rank: #${rank} \n Average: ${avg}€`
+                    );
+                })
+                .on("mouseout", function () {
+                    tooltip.style("visibility", "hidden");
+                })
+                .on("mousemove", function (event) {
+                    tooltip
+                        .style("top", event.pageY + 25 + "px")
+                        .style("left", event.pageX + 25 + "px");
+                });
 
             svg.append("g")
                 .selectAll("text")
