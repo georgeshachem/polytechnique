f378d7b9fef63fcdf67cee771ce290744ed4c95d
Georges Hachem <georges.hachem@france.polytechnique.fr>
2022-03-09 15:14:49 +0100

    first mpi commit

--- INF560/Project/src/main.c
+++ INF560/Project/src/main.c
@@ -10,6 +10,8 @@
 #include "gif_lib.h"
 #include "filters.c"
 
+#include <mpi.h>
+
 /*
  * Main entry point
  */
@@ -21,6 +23,13 @@ int main(int argc, char **argv)
     struct timeval t1, t2;
     double duration;
 
+    /* Start MPI Config */
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    /* End MPI Config */
+
     /* Check command-line arguments */
     if (argc < 3)
     {
@@ -28,61 +37,65 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    input_filename = argv[1];
-    output_filename = argv[2];
-
-    /* IMPORT Timer start */
-    gettimeofday(&t1, NULL);
-
-    /* Load file and store the pixels in array */
-    image = load_pixels(input_filename);
-    if (image == NULL)
+    if (rank == 0)
     {
-        return 1;
+        input_filename = argv[1];
+        output_filename = argv[2];
+
+        /* IMPORT Timer start */
+        gettimeofday(&t1, NULL);
+
+        /* Load file and store the pixels in array */
+        image = load_pixels(input_filename);
+        if (image == NULL)
+        {
+            return 1;
+        }
+
+        /* IMPORT Timer stop */
+        gettimeofday(&t2, NULL);
+
+        duration = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1e6);
+
+        printf("GIF loaded from file %s with %d image(s) in %lf s\n",
+               input_filename, image->n_images, duration);
+
+        /* FILTER Timer start */
+        gettimeofday(&t1, NULL);
+
+        /* Convert the pixels into grayscale */
+        apply_gray_filter_gif(image);
+
+        /* Apply blur filter with convergence value */
+        apply_blur_filter_gif(image, 5, 20);
+
+        /* Apply sobel filter on pixels */
+        apply_sobel_filter_gif(image);
+
+        /* FILTER Timer stop */
+        gettimeofday(&t2, NULL);
+
+        duration = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1e6);
+
+        printf("SOBEL done in %lf s\n", duration);
+
+        /* EXPORT Timer start */
+        gettimeofday(&t1, NULL);
+
+        /* Store file from array of pixels to GIF file */
+        if (!store_pixels(output_filename, image))
+        {
+            return 1;
+        }
+
+        /* EXPORT Timer stop */
+        gettimeofday(&t2, NULL);
+
+        duration = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1e6);
+
+        printf("Export done in %lf s in file %s\n", duration, output_filename);
     }
 
-    /* IMPORT Timer stop */
-    gettimeofday(&t2, NULL);
-
-    duration = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1e6);
-
-    printf("GIF loaded from file %s with %d image(s) in %lf s\n",
-           input_filename, image->n_images, duration);
-
-    /* FILTER Timer start */
-    gettimeofday(&t1, NULL);
-
-    /* Convert the pixels into grayscale */
-    apply_gray_filter_gif(image);
-
-    /* Apply blur filter with convergence value */
-    apply_blur_filter_gif(image, 5, 20);
-
-    /* Apply sobel filter on pixels */
-    apply_sobel_filter_gif(image);
-
-    /* FILTER Timer stop */
-    gettimeofday(&t2, NULL);
-
-    duration = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1e6);
-
-    printf("SOBEL done in %lf s\n", duration);
-
-    /* EXPORT Timer start */
-    gettimeofday(&t1, NULL);
-
-    /* Store file from array of pixels to GIF file */
-    if (!store_pixels(output_filename, image))
-    {
-        return 1;
-    }
-
-    /* EXPORT Timer stop */
-    gettimeofday(&t2, NULL);
-
-    duration = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1e6);
-
-    printf("Export done in %lf s in file %s\n", duration, output_filename);
-
+    MPI_Finalize();
     return 0;
 }
