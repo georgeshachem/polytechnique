333f93b290b6083f8359376dac428f492218b356
Georges Hachem <georges.hachem@france.polytechnique.fr>
2022-03-15 23:20:17 +0100

    added more checks

--- INF560/Project/run_test_final.sh
+++ INF560/Project/run_test_final.sh
@@ -9,16 +9,22 @@ mkdir $OUTPUT_DIR 2>/dev/null
 export OMP_NUM_THREADS=6
 cuda=1
 node_number=1
-process_number=6
+process_number=1
+
+if [ $OMP_NUM_THREADS -le 0 ] || [ $node_number -le 0 ] || [ $process_number -le 0 ] 
+then
+    echo "Error: The value provided can't be 0!"
+    exit -1
+fi
 
 for i in $INPUT_DIR/*gif ; do
     DEST=$OUTPUT_DIR/`basename $i .gif`-sobel.gif
     echo "Running test on $i -> $DEST"
 
-if [ $OMP_NUM_THREADS -gt 1 ]
-then
-    salloc -n 1 ./sobelf $i $DEST $cuda $OMP_NUM_THREADS
-else
-    salloc -N $node_number -n $process_number mpirun ./sobelf $i $DEST $cuda $OMP_NUM_THREADS
-fi
+    if [ $OMP_NUM_THREADS -gt 1 ]
+    then
+        salloc -n 1 ./sobelf $i $DEST $cuda $OMP_NUM_THREADS
+    else
+        salloc -N $node_number -n $process_number mpirun ./sobelf $i $DEST $cuda $OMP_NUM_THREADS
+    fi
 done
